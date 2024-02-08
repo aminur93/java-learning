1828906a3e57cbceb6f523955c1658f2a510a2a6
aminur93 <aminurrashid126@gmail.com>
2024-02-08 17:27:10 +0600

    java data types

---
+++ DataType.java
@@ -0,0 +1,20 @@
+public class DataType {
+    
+
+    public static void main (String args[])
+    {
+        float num1 = 10.5f;
+
+        double num2 = 45.55;
+
+        String first_name = "john";
+
+        String last_name = "doe";
+
+        System.out.println(first_name + " " + last_name);
+
+        System.out.println(num1);
+
+        System.out.println(num2);
+    } 
+}
