ab49882dbd620c87d6a01aedaed4efa8168460e8
aminur93 <aminurrashid126@gmail.com>
2024-02-08 16:59:46 +0600

    java variable

---
+++ Variable.java
@@ -0,0 +1,17 @@
+class Variable {
+
+    public static void main(String a[])
+    {
+        int num1 = 3;
+
+        int num2 = 5;
+
+        int result = num1 + num2;
+
+        String name = "Java";
+
+        System.out.println(" Hello " + name + " World!");
+
+        System.out.println(" Sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
