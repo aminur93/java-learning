0cd7f4805471653dbdb2d8e7c8df6ed0e3d18eb2
aminur93 <aminurrashid126@gmail.com>
2024-02-09 15:56:18 +0600

    java relational operator

---
+++ RelationalOperator.java
@@ -0,0 +1,40 @@
+public class RelationalOperator {
+
+    public static void main(String args[])
+    {
+        int x = 6;
+        int y = 5;
+
+        int a = 10;
+        int b = 10;
+
+        boolean result = x < y;
+
+        boolean result1 = x > y;
+
+        boolean result2 = a > b;
+
+        boolean result3 = a >= b;
+
+        boolean result4 = a <= b;
+
+        boolean result5 = a == b;
+
+        boolean result6 = x != y;
+
+        System.out.println("Result is: " + result);
+
+        System.out.println("Result1 is: " + result1);
+
+        System.out.println("Result2 is: " + result2);
+
+        System.out.println("Result3 is: " + result3);
+
+        System.out.println("Result4 is: " + result4);
+
+        System.out.println("Result5 is: " + result5);
+
+        System.out.println("Result6 is: " + result6);
+    }
+    
+}
