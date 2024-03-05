5a9ce7aa6f3907b9d80b97bc3086027232dba7b7
aminur93 <aminurrashid126@gmail.com>
2024-03-05 14:30:51 +0600

    enum if and switch case example of java

--- Enum/Demo.java
+++ Enum/Demo.java
@@ -8,14 +8,54 @@ public class Demo {
     
     public static void main(String args[])
     {
-        Status s = Status.REJECTED;
-        System.out.println(s);
+        Status s = Status.PENDING;
 
-        Status[] arr = Status.values();
 
-        for(Status a : arr)
-        {
-            System.out.println(a + " at index " + a.ordinal());
+        switch (s) {
+            case PENDING:
+                System.out.println("Pending your request");
+                break;
+
+            case APPROVED:
+                System.out.println("Approved your request");
+                break;
+
+            case REJECTED:
+                System.out.println("Rejected your request");
+                break;
+        
+            default:
+                System.out.println("Invalid Status");
+                break;
         }
+
+        /* conditional enum status*/
+        
+        // if (s == Status.PENDING) {
+
+        //     System.out.println("Pending your request");
+
+        // } else if (s == Status.APPROVED) {
+
+        //     System.out.println("Approved your request");
+
+        // } else if (s == Status.REJECTED) {
+
+        //     System.out.println("Rejected your request");
+
+        // } else {
+        //     System.out.println("Invalid Status");
+        // }
+
+        /* conditional enum status*/
+
+        /* print all status value */
+        // Status[] arr = Status.values();
+
+        // for(Status a : arr)
+        // {
+        //     System.out.println(a + " at index " + a.ordinal());
+        // }
+        /* print all status value */
     }
 }
